43b5ae20cf1aeab7107a2744a4b6e0187dd8ba28
project demo <parteraj2005@gmail.com>
2026-04-08 14:49:25 +0530

    update function .js file

---
+++ function.js
@@ -0,0 +1,32 @@
+//name function
+
+function meth(){
+   let a=10;
+   console.log(a);
+}
+ meth();
+
+
+ //anonymous function
+//1st way
+  var b =20;
+ const a = function (){
+  console.log(b);
+    
+ }
+a()
+//2nd way
+
+const q = function (){
+    let x = 30;
+    console.log(x);
+}
+q()
+
+
+//arrow function
+
+const x = ()=>{
+    console.log("object");
+}
+x()
