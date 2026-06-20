cd98187633a2c39c072d4a762ac6794e2dba758e
project demo <parteraj2005@gmail.com>
2026-06-20 21:38:57 +0530

    First commit

---
+++ function.js
@@ -0,0 +1,19 @@
+//function with name
+function meth(){
+    console.log("hello js");
+}
+meth()
+
+//Anonymous Function
+
+const a =function(){
+  console.log("hello unknown");
+}
+a()
+
+//arrow function
+
+const b = ()=>{
+    console.log("hello arrow function");
+}
+b()
---
+++ variables.js
@@ -0,0 +1,17 @@
+//let ,var ,const
+
+for(var i=0 ;i<5 ;i++){
+    console.log(i);
+}
+console.log(i);
+
+//let
+for(let i=0 ;i<5 ;i++){
+    console.log(i);
+}
+
+//const
+const a =39
+console.log(a);
+a=43
+console.log(a);
