78e02cf78ad64bc1973bb9ee77408f2c1d897c18
project demo <parteraj2005@gmail.com>
2026-06-30 20:55:42 +0530

    q2 file modify

--- q2.ts
+++ q2.ts
@@ -10,4 +10,12 @@ class Student{
 class School{
     static totalStudent:any =0;
     studentList:student[]=[]
+    addStudent(student:Student):void{
+        this.studentList.push(student)
+        School.totalStudent++
+    }
+ 
+    static totalCount():Number{
+        return School.totalStudent
+    }
 }
